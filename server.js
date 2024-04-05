9f9d508e99fb70c35c7efb3e0ae4a5999b42a02f
Jules MAISONNAVE <jules.maisonnave@student.junia.com>
2024-04-05 14:46:37 +0200

    update

--- server.js
+++ server.js
@@ -116,31 +116,31 @@ app.get('/files', async (req, res) => {
       res.status(500).send('Erreur lors de la récupération des données');
     }
   });
-app.get('/download', async (req, res) => {
-  const filePath = req.query.path;
-  const ecoCloudDownloadUrl = `https://739c-185-223-151-250.ngrok-free.app/download?path=${encodeURIComponent(filePath)}`;
-
-  try {
-    const response = await axios({
-      method: 'GET',
-      url: ecoCloudDownloadUrl,
-      responseType: 'stream',
-    });
-
-    res.setHeader('Content-Disposition', response.headers['content-disposition']);
-    res.setHeader('Content-Type', response.headers['content-type']);
-
-    // Utilisez `pipelineAsync` ici avec des streams valides
-    await pipelineAsync(response.data, res).catch(err => {
-      console.error('Erreur lors du stream du fichier:', err);
-      res.status(500).send('Erreur lors du stream du fichier');
-    });
-
-  } catch (error) {
-    console.error('Erreur lors du téléchargement:', error);
-    res.status(500).send('Erreur lors du téléchargement du fichier ou du dossier');
-  }
-});
+  app.get('/download', async (req, res) => {
+    const filePath = req.query.path;
+    const ecoCloudDownloadUrl = `https://739c-185-223-151-250.ngrok-free.app/download?path=${encodeURIComponent(filePath)}`;
+  console.log('/download',ecoCloudDownloadUrl)
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: ecoCloudDownloadUrl,
+        responseType: 'stream',
+      });
+  
+      res.setHeader('Content-Disposition', response.headers['content-disposition']);
+      res.setHeader('Content-Type', response.headers['content-type']);
+  
+      // Utilisez `pipelineAsync` ici avec des streams valides
+      await pipelineAsync(response.data, res).catch(err => {
+        console.error('Erreur lors du stream du fichier:', err);
+        res.status(500).send('Erreur lors du stream du fichier');
+      });
+  
+    } catch (error) {
+      console.error('Erreur lors du téléchargement:', error);
+      res.status(500).send('Erreur lors du téléchargement du fichier ou du dossier');
+    }
+  });
 
 app.listen(3000, '0.0.0.0', () => {
   console.log('Serveur démarré sur http://207.180.204.159:3000');
